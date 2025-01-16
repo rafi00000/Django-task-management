220d7a59f1d31404b8b723de52e6c391a90c8084
rafi00000 <delwar2021bd@gmail.com>
2025-01-16 12:56:35 +0600

    added postgress and created task and taskdetail model

--- task_management/settings.py
+++ task_management/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_management',
+        'USER': 'postgres',
+        'PASSWORD': 'admin1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -118,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- tasks/views.py
+++ tasks/views.py
@@ -9,3 +9,12 @@ def user_dashboard(request):
 def admin_dashboard(request):
     return render(request, 'dashboard/admin-dashboard.html')
 
+
+def context_req(request):
+    context = {
+        'names': ['John', 'Doe', 'Jane'],
+        'email': 'john@gmail.com',
+        'phone': '1234567890',
+        'address': '123, Main Street, City, Country'
+    }
+    return render(request, 'context.html', context)
